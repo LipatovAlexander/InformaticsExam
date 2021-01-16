2ea210b4b0892b0b6bef8ef8f9361cfdd8ae6337
Lipatov Alexander <lipatov.work@bk.ru>
2021-01-16 15:16:10 +0300

    Add 802

--- Tasks/Tasks802-821.cs
+++ Tasks/Tasks802-821.cs
@@ -6,6 +6,26 @@ namespace Tasks
 {
     public class Tasks802_821
     {
+        public int Task802(string text)
+        {
+            int maxCount = 0;
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                    count++;
+                else
+                {
+                    if (count > maxCount)
+                        maxCount = count;
+                    count = 0;
+                }
+            }
 
+            if (count > maxCount)
+                maxCount = count;
+
+            return maxCount;
+        }
     }
 }
--- Tests/TestTasks802-821.cs
+++ Tests/TestTasks802-821.cs
@@ -14,5 +14,11 @@ namespace Tests
         {
             _tasks802821 = new Tasks802_821();
         }
+
+        [TestCase("abcd   1 12  b1234", 4)]
+        public void Test802(string text, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _tasks802821.Task802(text));
+        }
     }
 }
